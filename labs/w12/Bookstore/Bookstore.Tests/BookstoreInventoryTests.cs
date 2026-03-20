6b6a412494aa3c52d156f423208842d464c7fda0
bjornnewton12 <bjorn.knutsson@edu.newton.se>
2026-03-20 11:17:23 +0100

    Added 2 unit tests: TestAddBook(), TestRemoveBook()

--- labs/w12/Bookstore/Bookstore.Tests/BookstoreInventoryTests.cs
+++ labs/w12/Bookstore/Bookstore.Tests/BookstoreInventoryTests.cs
@@ -15,10 +15,43 @@ public class BookstoreInventoryTests
     }
 
     [TestMethod]
-    public void Test1()
+    public void TestAddBook()
     {
-        //Implement tests
-        Assert.IsTrue(true);
+        // --- Arrange ---
+        // I need a book
+        var isbn = "123";
+        var expectedStock = 5;
+        var bestBook = new Bookstore.Book(isbn, "The Best of the Books", "Jane Doe", expectedStock);
+
+        // --- Act ---
+        // Add the book to the library
+        _inventory.AddBook(bestBook);
+
+        // --- Assert ---
+        // Library should have book in it's inventory
+        var stock = _inventory.CheckStock(isbn);
+
+        Assert.AreEqual(expectedStock, stock);
     }
 
+    [TestMethod]
+    public void TestRemoveBook()
+    {
+        // --- Arrange ---
+        // I need a book
+        var isbn = "321";
+        var initialStock = 5;
+        var worstBook = new Bookstore.Book(isbn, "The Worst of the Books", "John Doe", initialStock);
+
+        // --- Act ---
+        // Add book
+        _inventory.AddBook(worstBook);
+
+        // Remove book
+        var remove = _inventory.FindBookByTitle("The Worst of the Books");
+        _inventory.RemoveBook(remove.ISBN);
+
+        // --- Assert ---
+        Assert.AreEqual(initialStock - 1, remove.Stock);
+    }
 }
