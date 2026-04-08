38391e0415b8dca6f10f01f905c908e3b522dd23
bjornnewton12 <bjorn.knutsson@edu.newton.se>
2026-04-08 12:00:58 +0200

    Updated and tested User.cs and Product.cs

--- labs/w15/ShoppingCartAppIntegration.Tests/Product.cs
+++ labs/w15/ShoppingCartAppIntegration.Tests/Product.cs
@@ -1,20 +1,130 @@
 ﻿namespace ShoppingCartAppIntegration.Tests;
 
+using System.Net.Http;
+using System.Text.Json;
+using System.Net;
+using System.Text;
+
 [TestClass]
 public class Product
 {
-    [TestMethod]
-    public void AdminAddsProductToTheCatalog()
+    private readonly string _appUrl = GlobalContext.appUrl;
+    private readonly HttpClient _httpClient = new HttpClient();
+
+    private string randomString(int length)
     {
-        //Implement tests
-        Assert.IsTrue(false, "Test not implemented yet.");
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        var random = new Random();
+        return new string(Enumerable.Repeat(chars, length)
+          .Select(s => s[random.Next(s.Length)]).ToArray());
     }
 
-
+    /*
+    Given I am an admin user​
+    When I add a product to the catalog​
+    Then The product is available to be used in the app
+    */
     [TestMethod]
-    public void AdminRemovesProductFromTheCatalog()
+    public async Task TestAdminAddsProductToTheCatalog()
     {
-        //Implement tests
-        Assert.IsTrue(false, "Test not implemented yet.");
+        // Arrange (Given I am an admin user​)
+
+        // Login as admin
+        var loginResponse = await _httpClient.PostAsync($"{_appUrl}/login", new StringContent(
+            JsonSerializer.Serialize(new { username = "admin", password = "admin" }),
+            Encoding.UTF8,
+            "application/json"
+        ));
+
+        // Extract access token from login response
+        var loginContent = await loginResponse.Content.ReadAsStringAsync();
+        var loginJson = JsonDocument.Parse(loginContent).RootElement;
+        var adminToken = loginJson.GetProperty("access_token").GetString();
+
+        // Create product
+        var product = "product_" + randomString(8);
+
+        // Act (When I add a product to the catalog​)
+
+        // Build request with auth token and product name
+        var request = new HttpRequestMessage(HttpMethod.Post, $"{_appUrl}/product");
+        request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", adminToken);
+        request.Content = new StringContent(
+            JsonSerializer.Serialize(new { name = product }),
+            Encoding.UTF8,
+            "application/json"
+            );
+
+        var response = await _httpClient.SendAsync(request);
+
+        // Assert (Then The product is available to be used in the app)
+
+        // Check that the response is successful
+        Assert.IsTrue(response.StatusCode == HttpStatusCode.OK || 
+            response.StatusCode == HttpStatusCode.Created);
+
+        // Check that the response contains the expected data (product)
+        var responseContent = await response.Content.ReadAsStringAsync();
+        var jsonResponse = JsonDocument.Parse(responseContent).RootElement;
+        Assert.AreEqual(product, jsonResponse.GetProperty("name").GetString());
+    }
+
+    /*
+    Given I am an admin user​
+    When I delete a product from the catalog
+    Then The product is deleted
+    */
+    [TestMethod]
+    public async Task TestAdminRemovesProductFromTheCatalog()
+    {
+        // Arrange (Given I am an admin user​)
+
+        // Create name for product to be deleted
+        var productToDelete = "product_" + randomString(8);
+
+        // Login as admin
+        var loginResponse = await _httpClient.PostAsync($"{_appUrl}/login", new StringContent(
+            JsonSerializer.Serialize(new { username = "admin", password = "admin" }),
+            Encoding.UTF8,
+            "application/json"
+        ));
+
+        // Extract access token from login response
+        var loginContent = await loginResponse.Content.ReadAsStringAsync();
+        var loginJson = JsonDocument.Parse(loginContent).RootElement;
+        var adminToken = loginJson.GetProperty("access_token").GetString();
+
+        // Act (When I delete a product from the catalog)
+
+        // Create product to delete
+        var createRequest = new HttpRequestMessage(HttpMethod.Post, $"{_appUrl}/product");
+        createRequest.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", adminToken);
+        createRequest.Content = new StringContent(
+            JsonSerializer.Serialize(new { name = productToDelete }),
+            Encoding.UTF8,
+            "application/json"
+            );
+        var createResponse = await _httpClient.SendAsync(createRequest);
+
+        // Extract product id from createResponse
+        var createContent = await createResponse.Content.ReadAsStringAsync();
+        var createJson = JsonDocument.Parse(createContent).RootElement;
+        var productId = createJson.GetProperty("id").GetInt32();
+
+        // Delete product using its id
+        var deleteRequest = new HttpRequestMessage(HttpMethod.Delete, $"{_appUrl}/product/{productId}");
+        deleteRequest.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("Bearer", adminToken);
+        var response = await _httpClient.SendAsync(deleteRequest);
+
+        // Assert (Then The product is deleted)
+
+        // Check that the response is successful
+        Assert.IsTrue(response.StatusCode == HttpStatusCode.OK ||
+            response.StatusCode == HttpStatusCode.Created);
+
+        // Check that deleted product name matches
+        var responseContent = await response.Content.ReadAsStringAsync();
+        var jsonResponse = JsonDocument.Parse(responseContent).RootElement;
+        Assert.AreEqual(productToDelete, jsonResponse.GetProperty("name").GetString());
     }
 }
--- labs/w15/ShoppingCartAppIntegration.Tests/User.cs
+++ labs/w15/ShoppingCartAppIntegration.Tests/User.cs
@@ -1,29 +1,116 @@
 ﻿namespace ShoppingCartAppIntegration.Tests;
 
 using System.Net.Http;
-using System.Net.Http.Json;
+using System.Text.Json;
+using System.Net;
+using System.Text;
 
 [TestClass]
-public class User
+public sealed class ShoppingAppTests
 {
-    private static readonly HttpClient client = new HttpClient();
 
+    private readonly string _appUrl = GlobalContext.appUrl;
+    private readonly HttpClient _httpClient = new HttpClient();
 
-    [TestMethod]
-    public void RegisterNewCustomer()
+    private string randomString(int length)
     {
-        // Hint: Use appUrl from GlobalContext to make API calls to the application
-        // GlobalContext.appUrl
-
-
-        //Implement tests
-        Assert.IsTrue(false, "Test not implemented yet.");
+        const string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+        var random = new Random();
+        return new string(Enumerable.Repeat(chars, length)
+          .Select(s => s[random.Next(s.Length)]).ToArray());
     }
 
+    /*
+    Given I am a new potential customer
+    When I sign in in the app
+    Then I should be able to log in as an application customer
+    */
     [TestMethod]
-    public void CustomerListsProductsInCart()
+    public async Task TestCreateNewCustomer()
     {
-        //Implement tests
-        Assert.IsTrue(false, "Test not implemented yet.");
+        // Arrange
+
+        // Create username and password for the new customer
+        var username = "customer_" + randomString(8);
+        var password = "Password123!";
+
+        // Act
+
+        // Call signup endpoint
+        var response = await _httpClient.PostAsync($"{_appUrl}/signup", new StringContent(
+            JsonSerializer.Serialize(new { username, password }),
+            Encoding.UTF8,
+            "application/json"
+        ));
+
+        // Assert
+
+        // Check that the response is successful
+        Assert.AreEqual(HttpStatusCode.OK, response.StatusCode);
+
+        // Check that the response contains the expected data (username)
+        var responseContent = await response.Content.ReadAsStringAsync();
+        var jsonResponse = JsonDocument.Parse(responseContent).RootElement;
+        Assert.AreEqual(username, jsonResponse.GetProperty("username").GetString());
+
+        // Validate if the user can log in with the created credentials
+        var responseLogin = await _httpClient.PostAsync($"{_appUrl}/login", new StringContent(
+            JsonSerializer.Serialize(new { username, password }),
+            Encoding.UTF8,
+            "application/json"
+        ));
+        Assert.AreEqual(HttpStatusCode.OK, responseLogin.StatusCode);
+    }
+
+    /*
+    Given I am a customer​
+    When I log in into the application​
+    Then I should see all the products on my cart
+    */
+    [TestMethod]
+    public async Task TestCustomerListProductsInCart()
+    {
+        // Arrange (Given I am a customer​)
+
+        // Create username and password for the customer
+        var username = "customer_" + randomString(8);
+        var password = "Password123!";
+
+        // Sign up customer so there is a valid user to login
+        var signupResponse = await _httpClient.PostAsync($"{_appUrl}/signup", new StringContent(
+            JsonSerializer.Serialize(new { username, password }),
+            Encoding.UTF8,
+            "application/json"
+        ));
+
+        // Act (When I log in into the application​)
+
+        // Login as customer to get an access token
+        var loginResponse = await _httpClient.PostAsync($"{_appUrl}/login", new StringContent(
+            JsonSerializer.Serialize(new { username, password }),
+            Encoding.UTF8,
+            "application/json"
+        ));
+
+        // Extract access token from login response
+        var loginContent = await loginResponse.Content.ReadAsStringAsync();
+        var loginJson = JsonDocument.Parse(loginContent).RootElement;
+        var accessToken = loginJson.GetProperty("access_token").GetString();
+
+        // Call user endpoint with access token in Authorization header
+        var request = new HttpRequestMessage(HttpMethod.Get, $"{_appUrl}/user");
+        request.Headers.Authorization = new System.Net.Http.Headers.AuthenticationHeaderValue("bearer", accessToken);
+        var responseListProducts = await _httpClient.SendAsync(request);
+
+        // Assert (Then I should see all the products on my cart)
+
+        // Check if response is successful
+        Assert.AreEqual(HttpStatusCode.OK, signupResponse.StatusCode);
+        Assert.AreEqual(HttpStatusCode.OK, responseListProducts.StatusCode);
+
+        // Check that product list in the cart is empty for new customer
+        var responseContent = await responseListProducts.Content.ReadAsStringAsync();
+        var jsonResponse = JsonDocument.Parse(responseContent).RootElement;
+        Assert.AreEqual(0, jsonResponse.GetProperty("products").GetArrayLength());
     }
 }
